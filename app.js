f33a914001e84a1ed1b030b214fdd85111dc96a5
Prananda Yogathama Pribadi <120782165+prananda21@users.noreply.github.com>
2023-03-12 21:56:24 +0800

    Submission (1)

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+ 
+const express = require('express')
+ 
+const PORT = 3000
+const HOST = '0.0.0.0'
+ 
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Prananda Yogathama Pribadi\n')
+})
+ 
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
